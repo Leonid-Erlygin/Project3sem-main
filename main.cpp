0875512790ab9b96f446dfad0db4532f6aa45642
Leonid <erlygin.la@phystech.edu>
2018-11-17 21:24:52 +0300

    >added

--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@
 
 using namespace std;
 
+string getcmd(string &input) {//returns first word
+    string cmd = input.substr(0, input.find(' '));
+    return cmd;
+}
+
 string getCwd() {
     char *add = (char *) malloc(sizeof(char) * 10000);
     string s(getcwd(add, 10000));//расширить про очищение памяти в такой ситуации
     return s;
 }
 
-vector<char *> exclVector(string &cmd, string &input) {//формирует вектор аргументов(так как они пришли)
+//добавить к каждой входной строчке пробел в конце
+vector<char *> exclVector(string &input) {//формирует вектор аргументов(так как они пришли)
+    string cmd = getcmd(input);
     vector<string> placeholder;
     vector<char *> arg;
     placeholder.push_back(cmd);
@@ -32,10 +39,10 @@ vector<char *> exclVector(string &cmd, string &input) {//формирует ве
     arg.push_back(NULL);
     return arg;
 }
-
+//переделать
 void time(string &cmd, string &input) {
 
-    vector<char *> arg = exclVector(cmd, input);
+    vector<char *> arg = exclVector(input);
     arg.push_back(NULL);
 
     string a = "time";
@@ -71,9 +78,9 @@ void outRederection(string &command1, vector<char *> &args1, string &filename) {
         const char *name = filename.c_str();
         open(name, O_RDWR | O_CREAT | O_TRUNC, 0666);
         string path;
-        if(command1!="time"){path = "/bin/" + command1;}
-        else{
-           path = "/usr/bin/time";
+        if (command1 != "time") { path = "/bin/" + command1; }
+        else {
+            path = "/usr/bin/time";
         }
         execvp(path.c_str(), &args1[0]);
     } else {
@@ -82,13 +89,37 @@ void outRederection(string &command1, vector<char *> &args1, string &filename) {
     }
 }
 
-string getcmd(string &input) {//returns first word and truncates it
-    string cmd = input.substr(0, input.find(' '));
-    if (input.find(' ') + 1 != 0)
-        input = input.substr(input.find(' ') + 1, input.size());
-    else input = "";
-    return cmd;
+
+void f(string &s) {
+    close(0);
+    close(1);
+    int fd[2];
+    pipe(fd);
+    while (s.find('|') != -1) {
+        string cmd = s.substr(0, s.find('|'));
+        s = s.substr(0, s.find('|') + 1);
+        pid_t id = fork();
+        if (id == 0) {
+            string path = "/bin/" + getcmd(cmd);
+            vector<char *> arg = exclVector(cmd);
+            execvp(path.c_str(), &arg[0]);
+        } else {
+            continue;
+        }
+
+    }
 }
+
+void pipeline(string &s) {
+    pid_t id = fork();
+    if (id == 0) {
+        f(s);
+    } else {
+        int a;
+        waitpid(id, &a, 0);
+    }
+}
+
 
 int main(int argc, char **argv, char **envp) {
     /*for (int i = 0; i <argc; ++i) {
@@ -119,32 +150,37 @@ int main(int argc, char **argv, char **envp) {
             string s1 = input.substr(0, input.find('>'));
             string s2 = input.substr(input.find('>') + 1, input.size());
             string command1 = getcmd(s1);
-            vector <char *>arg = exclVector(command1,s1);
-            outRederection(command1,arg,s2);
+            vector<char *> arg = exclVector(s1);
+            outRederection(command1, arg, s2);
 
         } else {
-            string cmd = getcmd(input);
+            if (input.find('|') != -1) {
+
+                pipeline(input);
+
+            } else {
+                string cmd = getcmd(input);
 
 
-            if (cmd == "cd") {
-                cd(input);
-            } else if (cmd == "pwd") {
-                cout << dir;
-            } else if (cmd == "time") {//работает не всегда корректно
-                time(cmd, input);
-            } else {//внешняя команда
-                vector<char *> arg = exclVector(cmd, input);
-                string path = "/bin/" + cmd;
+                if (cmd == "cd") {
+                    cd(input);
+                } else if (cmd == "pwd") {
+                    cout << dir;
+                } else if (cmd == "time") {//работает не всегда корректно
+                    time(cmd, input);
+                } else {//внешняя команда
+                    vector<char *> arg = exclVector(input);
+                    string path = "/bin/" + cmd;
 
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execvp(path.c_str(), &arg[0]);
-                } else {
-                    int info;
-                    waitpid(pid, &info, 0);//родитель ждёт исполненя ребёнка
+                    pid_t pid = fork();
+                    if (pid == 0) {
+                        execvp(path.c_str(), &arg[0]);
+                    } else {
+                        int info;
+                        waitpid(pid, &info, 0);//родитель ждёт исполненя ребёнка
+                    }
                 }
             }
-
         }
     }
 
